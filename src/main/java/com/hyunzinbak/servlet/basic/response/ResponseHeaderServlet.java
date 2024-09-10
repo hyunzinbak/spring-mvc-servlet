2fa51376cec666eff616f5236d94166f463d6b35
hyunzinbak <hyunzinbak@gmail.com>
2024-09-10 22:55:46 +0900

    HTTP 응답 데이터 - API JSON * HTTP 응답으로 JSON을 반환할 때는 content-type을 application/json 로 지정해야 한다. * Jackson 라이브러리가 제공하는 objectMapper.writeValueAsString() 를 사용하면 객체를 JSON 문자로 변경할 수 있다.

--- src/main/java/com/hyunzinbak/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/com/hyunzinbak/servlet/basic/response/ResponseHeaderServlet.java
@@ -57,4 +57,20 @@ public class ResponseHeaderServlet extends HttpServlet {
         //response.setHeader("Location", "/basic/hello-form.html");
         response.sendRedirect("/basic/hello-form.html");
     }
+
+    @WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+    public static class ResponseHtmlServlet extends HttpServlet {
+        @Override
+        protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            //Content-Type: text/html;charset=utf-8
+            response.setContentType("text/html");
+            response.setCharacterEncoding("utf-8");
+            PrintWriter writer = response.getWriter();
+            writer.println("<html>");
+            writer.println("<body>");
+            writer.println(" <div>안녕?</div>");
+            writer.println("</body>");
+            writer.println("</html>");
+        }
+    }
 }
