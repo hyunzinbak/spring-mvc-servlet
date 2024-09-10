7253a98fd5b8104a352e51c2453c0c49b83cedac
hyunzinbak <hyunzinbak@gmail.com>
2024-09-10 22:36:05 +0900

    HTTP 요청 데이터 - API 메시지 바디 - JSON * content-type: application/json * JSON 결과를 파싱해서 사용할 수 있는 자바 객체로 변환하려면 Jackson, Gson 같은 JSON 변환 라이브러리를 추가해서 사용해야 한다. - 스프링 부트로 Spring MVC를 선택하면 기본으로 Jackson 라이브러리( ObjectMapper)를 함께 제공한다.

---
+++ src/main/java/com/hyunzinbak/servlet/basic/HelloData.java
@@ -0,0 +1,11 @@
+package com.hyunzinbak.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+    private String username;
+    private int age;
+}
