cd803e243a1649f601d08dffde5d8370cc755240
hyunzinbak <hyunzinbak@gmail.com>
2024-09-16 13:52:50 +0900

    섹션 6 : 스프링 MVC - 구조 이해 - 핸들러 매핑과 핸들러 어댑터

---
+++ src/main/java/com/hyunzinbak/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package com.hyunzinbak.servlet.web.springmvc.old;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handlerRequest");
+        return null;
+    }
+}
